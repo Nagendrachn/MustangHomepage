0345b47e6a357ee25c5d9099ffff7a1d0628f4d6
Nagendrachn <127941842+Nagendrachn@users.noreply.github.com>
2023-11-14 15:39:12 +0530

    Add files via upload

---
+++ functionmunstang.js
@@ -0,0 +1,15 @@
+function munstang() {
+    open("https://www.ford.com/cars/mustang/")
+}
+function home() {
+    open("https://www.ford.com/")
+}
+function Vehicles() {
+    open("https://www.ford.com/cars/mustang/models/?gnav=vhpnav-specs")
+}
+function Shop() {
+    open("https://shop.ford.com/showroom/?gnav=header-shop-bp&linktype=build#/")
+}
+function Support() {
+    open("https://www.ford.com/support/?gnav=header-support")
+}
